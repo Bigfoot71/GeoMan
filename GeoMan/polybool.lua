a8f227450495c42dc73def0f71187c9111ad702d
Le Juez Victor <90587919+Bigfoot71@users.noreply.github.com>
2022-11-20 18:49:45 +0000

    slight optimizations

--- GeoMan/polybool.lua
+++ GeoMan/polybool.lua
@@ -29,46 +29,11 @@ function table.copy(tab)
     return {unpack(tab)}
 end
 
+local path = string.gsub(..., "polybool", "", 1)
+local lineCross = require(path.."lineCross")
+
 local function getDist(x1,y1,x2,y2)
     return math.sqrt((x1-x2)^2+(y1-y2)^2)
-end
-
-local lineCross = function (x1,y1,x2,y2,x3,y3,x4,y4)
-
-    local a1 = y2 - y1;
-    local b1 = x1 - x2;
-    local c1 = x2 * y1 - x1 * y2;
-
-    local r3 = a1 * x3 + b1 * y3 + c1;
-    local r4 = a1 * x4 + b1 * y4 + c1;
-
-    if ( r3 ~= 0 and r4 ~= 0 and ((r3 >= 0 and r4 >= 0) or (r3 < 0 and r4 < 0))) then
-        return
-    end
-
-    local a2 = y4 - y3;
-    local b2 = x3 - x4;
-    local c2 = x4 * y3 - x3 * y4;
-
-    local r1 = a2 * x1 + b2 * y1 + c2;
-    local r2 = a2 * x2 + b2 * y2 + c2;
-
-    if (r1 ~= 0 and r2 ~= 0 and ((r1 >= 0 and r2 >= 0) or (r1 < 0 and r2 < 0))) then
-        return
-    end
-
-    local denom = a1 * b2 - a2 * b1;
-
-    if ( denom == 0 ) then
-        return true;
-    end
-
-    --local offset = denom < 0 and - denom / 2 or denom / 2;
-
-    local x = b1 * c2 - b2 * c1;
-    local y = a2 * c1 - a1 * c2;
-
-      return x / denom, y / denom
 end
 
 local pointContain = function(x,y,p)
